f7ecd73eb602320003a6b45c60aa771c646a0547
Lera <popenya.valera@gmail.com>
2025-01-25 13:27:31 +0100

    Init

---
+++ src/main/java/com/task/googlesearchadapter/GoogleSearchAdapterApplication.java
@@ -0,0 +1,13 @@
+package com.task.googlesearchadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleSearchAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleSearchAdapterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/task/googlesearchadapter/GoogleSearchAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.googlesearchadapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleSearchAdapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
